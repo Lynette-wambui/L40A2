b8df6b723a9208dd7cd66e6981a1ab3a65dd5d2d
LYNETTE WAMBUI <lynettemuraga47@gmail.com>
2025-04-26 18:29:34 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,11 @@
+num1 = 60
+num2 = 40
+
+print("Number 1", num1)
+print("Number 2", num2)
+
+# Arithmetic Operators
+print("Addition:", num1 + num2)
+print("Subtraction:", num2 - num1)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
